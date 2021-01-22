db92cce34e511a54ceb90dcc9243fb512e188443
Dylan <dylanrosser4@hotmail.com>
2021-01-22 14:53:58 -0500

    tested board A1

--- test/plot.py
+++ test/plot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-path = "A/01_15_2020/"
-filename = "logic_data.csv"
+path = "A2/01_22_2021/"
+filename = "1khz_sin.csv"
 data = pd.read_csv(path+filename)
 
 # specific data point:
@@ -13,10 +13,10 @@ data = pd.read_csv(path+filename)
 plt.figure()
 plt.plot(data['Time (s)'], data['Bus'])
 #plt.plot(data['Time (s)'][1000:10000], data['DIN 9'][1000:10000]*1024)
-plt.title("Output Code for a 1kHz Triangle Wave")
+plt.title("Output Code for a 50kHz Sine Wave, Vref=0.9, S_clk=2MHz")
 plt.xlabel("time (s)")
 plt.ylabel("Code")
-plt.savefig(path+"figure.png")
+plt.savefig(path+"Sine_50k_long.png")
 plt.show()
 
 
