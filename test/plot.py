d18bf0b82ec0d89fd4b1b4965a9b7dde3c384aad
Dylan <dylanrosser4@hotmail.com>
2021-02-15 11:39:45 -0500

    added tests for today word doc

--- test/plot.py
+++ test/plot.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-path = "A2/01_22_2021/"
+path = "C1/"
 filename = "1khz_sin.csv"
 data = pd.read_csv(path+filename)
 
@@ -13,7 +13,7 @@ data = pd.read_csv(path+filename)
 plt.figure()
 plt.plot(data['Time (s)'], data['Bus'])
 #plt.plot(data['Time (s)'][1000:10000], data['DIN 9'][1000:10000]*1024)
-plt.title("Output Code for a 50kHz Sine Wave, Vref=0.9, S_clk=2MHz")
+plt.title("Output Code for a 1kHz Sine Wave, Vref=0.9, S_clk=2MHz")
 plt.xlabel("time (s)")
 plt.ylabel("Code")
 plt.savefig(path+"Sine_50k_long.png")
